da76db76426bc8cb03238f78d7ac347087a00be9
Abby-Lopez <alopezcampos13@gmail.com>
2021-10-01 15:10:48 -0600

    Creación del proyecto

---
+++ app/src/main/java/com/example/tareitasxd/act_crearTarea.java
@@ -0,0 +1,14 @@
+package com.example.tareitasxd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act_crearTarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_crear_tarea);
+    }
+}
