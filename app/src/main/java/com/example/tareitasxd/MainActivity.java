a6370b61485ff0b51278049d31b87c27bbdbcc0d
Abby-Lopez <alopezcampos13@gmail.com>
2021-10-05 14:08:23 -0600

    Spiner Principal

--- app/src/main/java/com/example/tareitasxd/MainActivity.java
+++ app/src/main/java/com/example/tareitasxd/MainActivity.java
@@ -6,20 +6,30 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static SQLiteDatabase db; //Se define la base de datos.
     private static final String NOMBRE_BD = "MultiSQLite"; //Se define el nombre de la bd.
     private final String TABLA_TAREA = "tarea"; //Se define el nombre de la tabla.
     
     Button btnAddTarea;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spPrincipal);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categorias, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         btnAddTarea = findViewById(R.id.btnAddTarea);
 
@@ -31,5 +41,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
