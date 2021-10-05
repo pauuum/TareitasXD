a2ae652d582a98f829b935bfe98c3bf1810e692d
Abby-Lopez <alopezcampos13@gmail.com>
2021-10-04 21:36:38 -0600

    Tarea

--- app/src/main/java/com/example/tareitasxd/Modelo/AdapterTarea.java
+++ app/src/main/java/com/example/tareitasxd/Modelo/AdapterTarea.java
@@ -1,29 +1,56 @@
 package com.example.tareitasxd.Modelo;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AdapterTarea extends BaseAdapter {
 
+    Context context;
+    ArrayList<Tarea> list;
+
+    public AdapterTarea (Context context, ArrayList<Tarea> list) {
+        this.context = context;
+        this.list = list;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return list.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        if(view == null){
+            LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(R.layout.item_tarea);
+        }
+        TextView lbNombre = view.findViewById(R.id.lbUser);
+        TextView lbCarnet = view.findViewById(R.id.lbName);
+        TextView lbCarrera = view.findViewById(R.id.lbLastName);
+
+
+        lbNombre.setText(list.get(i).getUser());
+        lbCarnet.setText(list.get(i).getName());
+        lbCarrera.setText(list.get(i).getLastName());
+
+
+        return view;
     }
 }
 
--- app/src/main/java/com/example/tareitasxd/Modelo/Tarea.java
+++ app/src/main/java/com/example/tareitasxd/Modelo/Tarea.java
@@ -1,4 +1,78 @@
 package com.example.tareitasxd.Modelo;
 
-public class Tarea {
+
+
+public class Tarea   {
+    private String tituloTarea;
+    private String categoria;
+    private String fecha;
+    private  String hora;
+    private String descripcion;
+
+    public Tarea(String tituloTarea, String categoria, String fecha, String hora, String descripcion) {
+        this.tituloTarea = tituloTarea;
+        this.categoria = categoria;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.descripcion = descripcion;
+    }
+
+    public Tarea() {
+        this.tituloTarea = "";
+        this.categoria = "";
+        this.fecha = "";
+        this.hora = "";
+        this.descripcion = "";
+    }
+
+    public String getTituloTarea() {
+        return tituloTarea;
+    }
+
+    public void setTituloTarea(String tituloTarea) {
+        this.tituloTarea = tituloTarea;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" +
+                "tituloTarea='" + tituloTarea + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", fecha='" + fecha + '\'' +
+                ", hora='" + hora + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
 }
