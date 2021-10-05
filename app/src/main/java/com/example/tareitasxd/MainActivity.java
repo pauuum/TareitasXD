4bbf1e27ecae21b426540ed33146e21c74c84294
pauuum <paulafabiola04@gmail.com>
2021-10-04 23:28:04 -0600

    BD

--- app/src/main/java/com/example/tareitasxd/MainActivity.java
+++ app/src/main/java/com/example/tareitasxd/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.tareitasxd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static SQLiteDatabase db; //Se define la base de datos.
+    private static final String NOMBRE_BD = "MultiSQLite"; //Se define el nombre de la bd.
+    private final String TABLA_TAREA = "tarea"; //Se define el nombre de la tabla.
+    
     Button btnAddTarea;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAddTarea = findViewById(R.id.btnAddTarea);
 
         btnAddTarea.setOnClickListener(new View.OnClickListener() {
             @Override
