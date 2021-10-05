3c022884564f7e632f0303018b7097a349fcef7d
pauuum <paulafabiola04@gmail.com>
2021-10-05 14:03:01 -0600

    Spinner Crear

--- app/src/main/java/com/example/tareitasxd/act_prueba.java
+++ app/src/main/java/com/example/tareitasxd/act_prueba.java
@@ -3,12 +3,33 @@ package com.example.tareitasxd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class act_prueba extends AppCompatActivity {
+public class act_prueba extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_prueba);
+
+        Spinner spinner = findViewById(R.id.spCrear);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.categorias, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
